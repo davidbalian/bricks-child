b7ab0d8e284d1d5ba6f137f256ace663c192dc54
David Balyan <davidbalyan9@gmail.com>
2025-06-13 20:26:19 +0300

    fixed mispelling

--- includes/notifications/email-verification-notification-template.php
+++ includes/notifications/email-verification-notification-template.php
@@ -17,7 +17,7 @@ if (!defined('ABSPATH')) {
         <div class="text-and-icon">
             <span class="notice-icon"><i class="fas fa-envelope"></i></span>
             <span class="notice-text">
-                To receive notifications, activate your <strong><?php echo esc_html($user_email); ?></strong>
+                To receive notifications, activate <strong><?php echo esc_html($user_email); ?></strong>
             </span>
         </div>
         <button class="send-verification-btn" data-email="<?php echo esc_attr($user_email); ?>">
