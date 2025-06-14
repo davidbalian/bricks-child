bc3ccf6a82fa8801cb5e4f031fa2a12e44c16b7d
David Balyan <davidbalyan9@gmail.com>
2025-06-14 18:04:36 +0300

    made slider static for now

--- includes/shortcodes/car-gallery-slider.php
+++ includes/shortcodes/car-gallery-slider.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Shortcode for a Slick-based car gallery with thumbnails and lightbox.
+ * Shortcode for a static car gallery with hero image and thumbnail row.
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -90,71 +90,59 @@ function car_gallery_slider_shortcode( $atts ) {
 		 return '<!-- No valid images found to display in gallery -->';
 	}
 
-	// Enqueue scripts and styles for the gallery.
-	car_gallery_slider_enqueue_scripts();
+	// Enqueue styles for the static gallery.
+	car_gallery_static_enqueue_styles();
 
 	$image_count = count( $images );
 
 	ob_start();
 	?>
-	<div class="car-gallery-slider-wrapper">
-		<div class="main-gallery-slider">
-			<?php foreach ( $images as $image ) : ?>
-				<div>
-					<img src="<?php echo esc_url( $image['sizes']['medium_large'] ); ?>" alt="<?php echo esc_attr( get_the_title( $image['ID'] ) ); ?>">
+	<div class="car-gallery-static-wrapper">
+		<!-- Hero Image -->
+		<div class="hero-image-container">
+			<?php if ( isset( $images[0] ) ) : ?>
+				<img src="<?php echo esc_url( $images[0]['sizes']['medium_large'] ); ?>" alt="<?php echo esc_attr( get_the_title( $images[0]['ID'] ) ); ?>" class="hero-image">
+				<div class="image-count-overlay">
+					<i class="fas fa-camera"></i>
+					<?php echo $image_count; ?> photos
 				</div>
-			<?php endforeach; ?>
+			<?php endif; ?>
 		</div>
-		<div class="gallery-controls">
-			<div class="photo-counter">
-				<span class="current-slide">1</span>/<?php echo $image_count; ?> Photos
+
+		<!-- Thumbnail Row (3 images) -->
+		<?php if ( $image_count > 1 ) : ?>
+			<div class="thumbnail-row">
+				<?php 
+				$thumbnail_images = array_slice( $images, 1, 3 ); // Get next 3 images after hero
+				foreach ( $thumbnail_images as $image ) : 
+				?>
+					<div class="thumbnail-item">
+						<img src="<?php echo esc_url( $image['sizes']['thumbnail'] ); ?>" alt="<?php echo esc_attr( get_the_title( $image['ID'] ) ); ?>">
+					</div>
+				<?php endforeach; ?>
+				
+				<!-- Fill empty slots if less than 3 images -->
+				<?php 
+				$remaining_slots = 3 - count( $thumbnail_images );
+				for ( $i = 0; $i < $remaining_slots; $i++ ) : 
+				?>
+					<div class="thumbnail-item empty"></div>
+				<?php endfor; ?>
 			</div>
-		</div>
-        <button class="view-all-images">View all images</button>
-
-		<div class="thumbnail-gallery-slider">
-			<?php foreach ( $images as $image ) : ?>
-				<div>
-					<img src="<?php echo esc_url( $image['sizes']['thumbnail'] ); ?>" alt="<?php echo esc_attr( get_the_title( $image['ID'] ) ); ?>">
-				</div>
-			<?php endforeach; ?>
-		</div>
+		<?php endif; ?>
 	</div>
-
-    <div class="cgs-lightbox" style="display:none;">
-        <div class="cgs-lightbox-content">
-            <button class="cgs-close">&times;</button>
-            <div class="cgs-lightbox-slider">
-                <?php foreach ( $images as $image ) : ?>
-                    <div>
-                        <img src="<?php echo esc_url( $image['url'] ); ?>" alt="<?php echo esc_attr( get_the_title( $image['ID'] ) ); ?>">
-                    </div>
-                <?php endforeach; ?>
-            </div>
-        </div>
-    </div>
 
 	<?php
 	return ob_get_clean();
 }
 
 /**
- * Enqueue scripts and styles for the gallery.
+ * Enqueue styles for the static gallery.
  */
-function car_gallery_slider_enqueue_scripts() {
+function car_gallery_static_enqueue_styles() {
 	$theme_version = defined('BRICKS_CHILD_THEME_VERSION') ? BRICKS_CHILD_THEME_VERSION : '1.0.0';
 	$theme_dir_uri = get_stylesheet_directory_uri();
 
-	// Enqueue Slick CSS
-	wp_enqueue_style( 'slick-css', 'https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css', array(), '1.8.1' );
-	wp_enqueue_style( 'slick-theme-css', 'https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css', array( 'slick-css' ), '1.8.1' );
-
 	// Enqueue Custom CSS
-	wp_enqueue_style( 'car-gallery-slider-css', $theme_dir_uri . '/assets/css/car-gallery-slider.css', array(), $theme_version );
-
-	// Enqueue Slick JS
-	wp_enqueue_script( 'slick-js', 'https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js', array( 'jquery' ), '1.8.1', true );
-
-	// Enqueue Custom JS
-	wp_enqueue_script( 'car-gallery-slider-js', $theme_dir_uri . '/assets/js/car-gallery-slider.js', array( 'jquery', 'slick-js' ), $theme_version, true );
+	wp_enqueue_style( 'car-gallery-static-css', $theme_dir_uri . '/assets/css/car-gallery-static.css', array(), $theme_version );
 } 
