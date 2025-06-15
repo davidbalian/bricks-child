0e4440006314f4f22dfe2c0229fcb64a51dc5b4a
llkotidis <leonidaskotidis4@gmail.com>
2025-06-15 13:25:05 +0300

    Update single-car-page.php

--- includes/shortcodes/single-car-page/single-car-page.php
+++ includes/shortcodes/single-car-page/single-car-page.php
@@ -33,8 +33,8 @@ function single_car_buttons_shortcode($atts) {
     }
 
     // Enqueue styles and scripts
-    wp_enqueue_style('single-car-page-css', get_template_directory_uri() . '/includes/shortcodes/single-car-page/single-car-page.css', array(), '1.0.0');
-    wp_enqueue_script('single-car-buttons-js', get_template_directory_uri() . '/includes/shortcodes/single-car-page/single-car-buttons.js', array('jquery'), '1.0.0', true);
+    wp_enqueue_style('single-car-page-css', get_stylesheet_directory_uri() . '/includes/shortcodes/single-car-page/single-car-page.css', array(), '1.0.0');
+    wp_enqueue_script('single-car-buttons-js', get_stylesheet_directory_uri() . '/includes/shortcodes/single-car-page/single-car-buttons.js', array('jquery'), '1.0.0', true);
 
     // Get current user info for favorites
     $current_user_id = get_current_user_id();
