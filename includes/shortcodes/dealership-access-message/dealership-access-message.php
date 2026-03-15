2bc94371fa4e9ca429585c6b2c9bbc6c4deaa0cf
llkotidis <leonidaskotidis4@gmail.com>
2026-03-15 19:45:47 +0200

    Update dealership-access-message.php

--- includes/shortcodes/dealership-access-message/dealership-access-message.php
+++ includes/shortcodes/dealership-access-message/dealership-access-message.php
@@ -81,18 +81,6 @@ function dealership_access_message_shortcode($atts) {
         $autoagora_dealer = get_user_meta($user_id, 'autoagora_dealer', true);
     }
     
-    // Get all user meta keys that contain "autoagora" for debugging
-    $all_user_meta = get_user_meta($user_id);
-    $autoagora_meta = array();
-    foreach ($all_user_meta as $key => $value) {
-        if (strpos($key, 'autoagora') !== false) {
-            $autoagora_meta[$key] = $value;
-        }
-    }
-    
-    // Also check for ACF field object to see field settings
-    $field_object = get_field_object('autoagora_dealer', 'user_' . $user_id);
-    
     // Convert to a comparable value - normalize to integer for comparison
     $dealer_value = null;
     if ($autoagora_dealer !== null && $autoagora_dealer !== '' && $autoagora_dealer !== false) {
@@ -100,40 +88,14 @@ function dealership_access_message_shortcode($atts) {
         $dealer_value = intval($autoagora_dealer);
     }
     
-    // TEMPORARY DEBUG - Remove this block after testing
-    // This will show visible debug info on the page
-    $debug_output = '<div style="background: #ffeb3b; padding: 10px; margin: 10px 0; border: 2px solid #f57f17;">';
-    $debug_output .= '<strong>DEBUG INFO:</strong><br>';
-    $debug_output .= 'Post ID: ' . esc_html($post_id) . '<br>';
-    $debug_output .= 'User ID: ' . esc_html($user_id) . '<br>';
-    $debug_output .= 'ACF get_field(user): ' . var_export($autoagora_dealer, true) . ' (type: ' . gettype($autoagora_dealer) . ')<br>';
-    $debug_output .= 'Direct get_user_meta(): ' . var_export(get_user_meta($user_id, 'autoagora_dealer', true), true) . '<br>';
-    $debug_output .= 'ACF Field Object exists: ' . ($field_object ? 'YES' : 'NO') . '<br>';
-    if ($field_object) {
-        $debug_output .= 'Field Object name: ' . esc_html($field_object['name'] ?? 'N/A') . '<br>';
-        $debug_output .= 'Field Object key: ' . esc_html($field_object['key'] ?? 'N/A') . '<br>';
-        $debug_output .= 'Field Object value: ' . var_export($field_object['value'] ?? 'N/A', true) . '<br>';
-    }
-    $debug_output .= '<strong>All autoagora user meta keys:</strong><br>';
-    if (empty($autoagora_meta)) {
-        $debug_output .= '&nbsp;&nbsp;<em>No user meta keys found containing "autoagora"</em><br>';
-    } else {
-        foreach ($autoagora_meta as $key => $value) {
-            $debug_output .= '&nbsp;&nbsp;' . esc_html($key) . ' = ' . var_export($value, true) . '<br>';
-        }
-    }
-    $debug_output .= 'Dealer Value (intval): ' . var_export($dealer_value, true) . '<br>';
-    $debug_output .= 'Will show message: ' . ($dealer_value === 1 ? 'NO' : 'YES') . '<br>';
-    $debug_output .= '</div>';
-    
     // Only show message if value is NOT 1 (i.e., it's 0, null, empty, false, or not set)
     // If dealer_value is 1, the dealer is claimed - don't show message
     if ($dealer_value === 1) {
-        return $debug_output; // Show debug when claimed (should not show message)
+        return '';
     }
     
     // Dealer is NOT claimed (value is 0, null, empty, false, or not set), so show the message
-    return $debug_output . get_dealership_access_message_html();
+    return get_dealership_access_message_html();
 }
 
 /**
